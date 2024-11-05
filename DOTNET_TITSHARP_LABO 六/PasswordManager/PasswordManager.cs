c47e6b74345cff02831cf131fc3965913664385d
mayonezny <borischubuhchiev@mail.ru>
2024-11-05 20:22:23 +0300

    Добавьте файлы проекта.

---
+++ DOTNET_TITSHARP_LABO 六/PasswordManager/PasswordManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DOTNET_TITSHARP_LABO_六.PasswordManager
+{
+    public class PasswordManager
+    {
+
+    }
+
+
+}
+
+
---
+++ DOTNET_TITSHARP_LABO 六/UI/UI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNET_TITSHARP_LABO_六.UI
+{
+    internal class UI
+    {
+    }
+}
