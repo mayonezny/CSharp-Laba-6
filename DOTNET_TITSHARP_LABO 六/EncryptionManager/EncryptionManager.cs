8e8c4184641c1e0401d40657a78dd184a360c5cb
mayonezny <borischubuhchiev@mail.ru>
2024-11-05 20:36:52 +0300

    added class EncryptionManager

--- DOTNET_TITSHARP_LABO 六/EncryptionManager/EncryptionManager.cs
+++ DOTNET_TITSHARP_LABO 六/EncryptionManager/EncryptionManager.cs
@@ -1,12 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace DOTNET_TITSHARP_LABO_六.EncryptionManager
 {
-    internal class EncryptionManager
+    public static class EncryptionManager
     {
+        private static readonly byte[] Key = Encoding.UTF8.GetBytes("Your32CharSecretKeyHere!");
+        private static readonly byte[] IV = Encoding.UTF8.GetBytes("Your16CharIVHere");
+
+        public static string Encrypt(string plainText)
+        {
+            using (var aes = Aes.Create())
+            {
+                aes.Key = Key;
+                aes.IV = IV;
+                var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                byte[] encrypted;
+                using (var ms = new System.IO.MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var sw = new System.IO.StreamWriter(cs))
+                        {
+                            sw.Write(plainText);
+                        }
+                        encrypted = ms.ToArray();
+                    }
+                }
+                return Convert.ToBase64String(encrypted);
+            }
+        }
+
+        public static string Decrypt(string cipherText)
+        {
+            using (var aes = Aes.Create())
+            {
+                aes.Key = Key;
+                aes.IV = IV;
+                var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                string plainText;
+                using (var ms = new System.IO.MemoryStream(Convert.FromBase64String(cipherText)))
+                {
+                    using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var sr = new System.IO.StreamReader(cs))
+                        {
+                            plainText = sr.ReadToEnd();
+                        }
+                    }
+                }
+                return plainText;
+            }
+        }
     }
+
 }
